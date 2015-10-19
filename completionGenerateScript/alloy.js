fd78a8a6eac0ba9f65e65e09d10ebb744c106cd1
Jong Eun Lee <yomybaby@yomybaby.com>
2015-10-19 20:49:37 +0900

    update alloy tag list generator

--- completionGenerateScript/alloy.js
+++ completionGenerateScript/alloy.js
@@ -1,12 +1,32 @@
 var fs = require('fs');
 var _ = require('underscore');
-var api = JSON.parse(fs.readFileSync("/usr/local/lib/node_modules/alloy/docs/api.jsca", "utf8"));
+var alloyPath = '/usr/local/lib/node_modules/alloy'
+var api = JSON.parse(fs.readFileSync(alloyPath+"/docs/api.jsca", "utf8"));
 
-_.each(api.types,function(item,idx){
-  var suggestion = {
-    text : item.name,
-    description : item.description,
-    type : "object"
+// _.each(api.types,function(item,idx){
+//   var suggestion = {
+//     text : item.name,
+//     description : item.description,
+//     type : "object"
+//   }
+//   console.log(suggestion);
+// });
+
+
+///
+/// Generate Available Tag List
+var fns = fs.readdirSync(alloyPath+'/Alloy/commands/compile/parsers');
+var tagArr = []
+_.each(fns,function(fn) {
+  var ar = fn.split('.');
+  var tagName = ar[ar.length-2];
+  if(tagName.indexOf('_')!==0 && tagName[0] == tagName[0].toUpperCase()){
+    tagArr.push(tagName);
+  }else if(tagName=='_ProxyProperty' && fn.indexOf('Ti.UI')===0){
+    tagArr.push(ar[ar.length-3]); // Ti.UI.Window._ProxyProperty
   }
-  console.log(suggestion);
 });
+tagArr = _.uniq(tagArr).sort();
+
+console.log(tagArr.join('|')); // for tss.cson
+console.log(JSON.stringify(tagArr)); // for completions.json
